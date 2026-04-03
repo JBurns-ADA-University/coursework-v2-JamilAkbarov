8252abccb8483225c98f824e2aa7c083c8e8e5a1
JamilAkbarov <150743049+JamilAkbarov@users.noreply.github.com>
2026-04-03 13:28:03 +0400

    some changes applied

--- hw_1.s
+++ hw_1.s
@@ -1,37 +1,25 @@
 .global _start
-
 _start:
+    MOV r2, #0       # First of all we assign value 0 to sum  
 
-    MOV r2, #0              @ initialize sum to zero before loop begins
+    ADD r2, r2, r0    #then we start a loop where we add first value in range (r0,r1) to r2 which is sum variable      
+    ADD r0, r0, #1    #then we iterate a value r0 by 1 to move further in the range until r1, including r1 to the sum, since instructios require so      
+    CMP r0, r1        #Furthermore, we compare values r0 and r1 which would help us to set NZCV flags that are used in further instructions        
+    MOV r3, #1        # we assign r3 value 1 which would be needed futher      
+    LSL r3, r3, #2    #we move bit to the left 2 times, hence we get 0100 value which is converted to 4 which is technically bytes-per-word value, which is requested in the exercise      
+    MOV r4, #-32      # we assign value of -32 since it equals to the value of negative -32, -32 bytes = 8 instructions back (Each instruction is 4 bytes, in our case it goes backwards due to negative sign)
 
-    @ --- loop start (here the so called loop which we are utilizing in this program without actually using typical branching starts)--- 
-    ADD r2, r2, r0          @ add current counter value into running sum
+    ADDLE r5, pc, r4       
+    MOVGT r5, pc            
+    MOV pc, r5             
 
-    ADD r0, r0, #1          @ increment counter by 1 to move to the next integer
-
-    CMP r0, r1              @ subtract r1 from r0 and update NZCV condition flags which are connected with commands downwards such as MVNLE and MOVGT
-
-    @ --- compute bytes per word using shift ---
-
-    MOV r3, #1              @ load 1 into r3 (binary: 0001)
-
-    LSL r3, r3, #2          @ shift left 2 positions: 0001 becomes 0100 which equals 4
-
-    @I could have made it just MOV r3, #4 but decided to use some tricky technique
-
-    @ --- compute offset based on condition flags ---
-
-    MVNLE r4, #35           @ if r0 <= r1: r4 = NOT(35) = -36 in twos complement, jump back and -36 is  - 0x0000024 in hexadecimal
-
-    MOVGT r4, #0            @ if r0 > r1:  r4 = 0, no backwards jump, fall through to exit
-
-    @ --- compute target address and jump ---
-    ADD r5, pc, r4          @ r5 = pc + r4, pc reads as current address + 8 due to pipeline so in base case we are summing 0x0000028 + (- 0x0000024) which equal 0x000004 and hence jumps back to the start 
-                            @ however when r4 is 0 r5 if 0x00000028 hence jumping to the SVC 2 and ending program 
+    SVC 2          
 
 
-    MOV pc, r5              @ write target address into PC, redirecting execution
-
-
-    @ --- exit ---
-    SVC 2                   @ supervisor call, terminates the program on ARM Linux
+    # In the last subsection of 3 commands we add pc and r4 in case of r0 being less or equal to r1
+    # What is interesting here is current address(pc) being 2 commands ahead so in (MOV pc, r5)
+    # That works out because of specific pipeline architecture in ARM 
+    # This is a legacy behavior carried over from the original ARM design and remains consistent across ARM architecture to maintain backwards compatibility.
+    # So when you read pc inside an instruction, you always get current_address + 8, not the address of the instruction itself.
+    # Therefore to iterate backwards to the command (ADD r2,r2,r0) we need to add pc to -32 which would throw it 8 command back straighly to ADD r2, r2, r0, therefore we hardcode the value of -32 to r4 
+    # the iteration moves on until r0 is less or equal to r1, the moment it is more, the ADDLE r5, pc, r4  doesn't execute and we move it the next command which doesn't do anything with pc value hence the instruction proceeds and exits SVC 2
